6323ebb34a46287c601542792ea7e5d0bef6fa0a
bblu <gmwblu@gmail.com>
2019-06-27 19:20:05 +0800

    'up'

--- java/springframe/src/main/java/com/zoomway/patrol/PatrolApplication.java
+++ java/springframe/src/main/java/com/zoomway/patrol/PatrolApplication.java
@@ -1,12 +1,18 @@
 package com.zoomway.patrol;
 
+import com.zoomway.patrol.service.storage.StorageProperties;
+import com.zoomway.patrol.service.storage.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class PatrolApplication {
 
 	private final static Logger logger = LoggerFactory
@@ -15,4 +21,11 @@ public class PatrolApplication {
 		SpringApplication.run(PatrolApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
--- java/springframe/src/main/java/com/zoomway/patrol/controller/FileController.java
+++ java/springframe/src/main/java/com/zoomway/patrol/controller/FileController.java
@@ -3,6 +3,7 @@ package com.zoomway.patrol.controller;
 import com.zoomway.patrol.controller.common.HttpResult;
 import com.zoomway.patrol.entity.Task;
 import com.zoomway.patrol.service.TaskService;
+import com.zoomway.patrol.service.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //@EnableTransactionManagement
 
@@ -22,21 +24,28 @@ import java.util.List;
 @Validated
 public class FileController {
     private final static Logger logger = LoggerFactory.getLogger(FileController.class);
+    private final StorageService storageService;
+
+    @Autowired
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
     @Autowired
     TaskService service;
     /**
      * upload file by wblu @ 2019-06-23
      *
-     * @param files
+     * @param file
      */
     @PostMapping(value = "/upload")
-    public HttpResult upload(MultipartFile[] files)throws Exception{
+    public HttpResult upload(@RequestHeader(name = "X-Tower", required = true) Integer tower,
+                             MultipartFile file)throws Exception{
         HttpResult res = new HttpResult();
-        for (MultipartFile file : files) {
-            // ... 处理文件储存逻辑
-            logger.warn(file.getName());
-        }
-        return res.setData("ok");
+        storageService.store(file);
+        // ... 处理文件储存逻辑
+        logger.warn(file.getName());
+
+        return res.setData("ok"+ tower);
     }
 /**
  * 任务详情 by wblu @ 2019-05-28
